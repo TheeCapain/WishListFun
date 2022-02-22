35f65b7f7aaa92b8e7c36d65ea7770fe08926445
August <Augusthauerslev@outlook.com>
2022-02-22 12:40:35 +0100

    initial commit

---
+++ src/main/java/com/example/wishlistfun/WishListFunApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishlistfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListFunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishListFunApplication.class, args);
+    }
+
+}
